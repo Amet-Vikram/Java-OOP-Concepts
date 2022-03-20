d069e5ea5b10de25c033aaed794f2a8224b6f61c
Amet-Vikram <amet1197@gmail.com>
2022-03-20 18:15:34 +0530

    Account Management

---
+++ src/com/bridgelabz/oop/bankaccount/Account.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.oop.bankaccount;
+
+public class Account{
+     String name;
+     long balance;
+     Long accNumber;
+
+    public Account(String name, long accNumber, long balance) {
+        this.name = name;
+        this.balance = balance;
+        this.accNumber = accNumber;
+    }
+
+    public void debit(int debit) {
+        if(debit < balance){
+            balance -= debit;
+        }
+        else{
+            System.out.println("The Debit exceeds the Balance. Transaction not allowed!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", accNumber=" + accNumber +
+                ", balance=" + balance +
+                '}';
+    }
+}
